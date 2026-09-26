cb939d300d80908eca8de4c42088b9c771364ec9
LycorisRadia437 <radialycoris437@gmail.com>
2026-09-26 10:04:06 +0700

    Update Neptune library source URL

--- Kp.lua
+++ Kp.lua
@@ -70,7 +70,7 @@ local function TeleportToNamedItem(itemName)
 end
 
 -- Tải Thư viện UI Neptune gốc
-local Neptune = loadstring(game:HttpGet("https://raw.githubusercontent.com/JinxTheCatto/Neptune/refs/heads/main/Games/Dependencies/UniversInit.lua"))()
+local Neptune = loadstring(game:HttpGet("https://raw.githubusercontent.com/LycorisRadia437/My-Repository/refs/heads/main/Neptuneui.lua"))()
 
 -- Khởi tạo các Tab (Sử dụng 'text' thay vì 'Name' để map đúng với cấu trúc Neptune)
 local UniversalTab = Neptune:CreateTab({text = "Universal"})
